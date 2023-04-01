c733202af7ded1beac7a9220b8c7559b71ff53ed
RedStar6 <richmondnkrumah04@gmail.com>
2023-04-01 02:13:58 +0000

    Added New Images

--- Images/index.js
+++ Images/index.js
@@ -1,9 +1,11 @@
 import street1 from './street1.jpg'
 import street2 from './street2.jpg'
 import street3 from './street3.jpg'
+import dawning from './dawning.jpg'
 
 
 export default [
+    dawning,
     street1,
     street2,
     street3,
